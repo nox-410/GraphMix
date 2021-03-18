7693facb8875ebc3e551da59da4beb4e16f96a9a
Shi Yining <1700012728@pku.edu.cn>
2021-03-18 13:41:56 +0800

    remove psagent

--- ps-lite/include/ps/worker/worker.h
+++ ps-lite/include/ps/worker/worker.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "ps/ps.h"
-#include "ps/worker/PSAgent.h"
+#include "ps/worker/kvworker.h"
 
 using namespace ps;
 
 class Worker {
 public:
   Worker();
+  KVWorker _kvworker;
 
   // for data push&pull
   typedef uint64_t query_t;
@@ -20,15 +21,18 @@ public:
       an query_t which is a long
       use waitdata(query_t) to wait for its success
   */
-  query_t push_data(const long *indices, int index_size, float *data, const long *lengths);
+  query_t pushData(const long *indices, int index_size, float *data, const long *lengths);
   // this is almost the same as push_data
-  query_t pull_data(const long *indices, int index_size, float *data, const long *lengths);
+  query_t pullData(const long *indices, int index_size, float *data, const long *lengths);
   /*
     wait_data waits until a query success
   */
-  void wait_data(query_t query);
+  void waitData(query_t query);
 
 private:
+  void _pushData(Key idx, float *vals, int len, std::vector<int>& timestamp);
+  void _pullData(Key idx, float *vals, int len, std::vector<int>& timestamp);
+  void waitTimestamp(int timestamp) { _kvworker.Wait(timestamp); }
   // used this hold to thread_pool return object
   std::unordered_map<query_t, std::vector<int>> query2timestamp;
   // data_pull & data_push query, increase 1 each call
--- ps-lite/src/worker.cc
+++ ps-lite/src/worker.cc
@@ -1,56 +1,77 @@
 #include "ps/worker/worker.h"
 
-Worker::Worker() {}
+Worker::Worker() : _kvworker(0, 0) {}
 
-Worker::query_t Worker::push_data(const long *indices, int index_size, float *data, const long *lengths) {
-    data_mu.lock();
-    query_t cur_query = next_query++;
-    auto &timestamps = query2timestamp[cur_query];
-    data_mu.unlock();
+Worker::query_t Worker::pushData(const long* indices, int index_size, float* data, const long* lengths) {
+  data_mu.lock();
+  query_t cur_query = next_query++;
+  auto& timestamps = query2timestamp[cur_query];
+  data_mu.unlock();
 
-    for(int i = 0; i < index_size; i++) {
-        Key idx = (Key)indices[i];
-        auto len = lengths[i];
-        PSAgent::Get()->PushData(idx, data, len, timestamps);
-        data += len;
-    }
-    return cur_query;
+  for (int i = 0; i < index_size; i++) {
+    Key idx = (Key)indices[i];
+    auto len = lengths[i];
+    _pushData(idx, data, len, timestamps);
+    data += len;
+  }
+  return cur_query;
 }
 
 // this is almost the same as push_data
-Worker::query_t Worker::pull_data(const long *indices, int index_size, float *data, const long *lengths) {
-    data_mu.lock();
-    query_t cur_query = next_query++;
-    auto &timestamps = query2timestamp[cur_query];
-    data_mu.unlock();
+Worker::query_t Worker::pullData(const long* indices, int index_size, float* data, const long* lengths) {
+  data_mu.lock();
+  query_t cur_query = next_query++;
+  auto& timestamps = query2timestamp[cur_query];
+  data_mu.unlock();
 
-    for(int i = 0; i < index_size; i++) {
-        Key idx = (Key)indices[i];
-        auto len = lengths[i];
-        PSAgent::Get()->PullData(idx, data, len, timestamps);
-        data += len;
-    }
-    return cur_query;
+  for (int i = 0; i < index_size; i++) {
+    Key idx = (Key)indices[i];
+    auto len = lengths[i];
+    _pullData(idx, data, len, timestamps);
+    data += len;
+  }
+  return cur_query;
 }
 
 /*
     wait_data waits until a query success
 */
-void Worker::wait_data(query_t query) {
-    data_mu.lock();
-    auto iter = query2timestamp.find(query);
-    if (iter == query2timestamp.end()) {
-        data_mu.unlock();
-        LG << "Wait on empty query " << query;
-        return;
-    } else {
-        auto timestamps = std::move(iter->second);
-        query2timestamp.erase(iter);
-        data_mu.unlock();
-        for(int t: timestamps) {
-            PSAgent::Get()->waitTimestamp(t);
-        }
+void Worker::waitData(query_t query) {
+  data_mu.lock();
+  auto iter = query2timestamp.find(query);
+  if (iter == query2timestamp.end()) {
+    data_mu.unlock();
+    LG << "Wait on empty query " << query;
+    return;
+  } else {
+    auto timestamps = std::move(iter->second);
+    query2timestamp.erase(iter);
+    data_mu.unlock();
+    for (int t : timestamps) {
+      waitTimestamp(t);
     }
+  }
+}
+
+Key mapWkeyToSkey(Key idx) {
+  const std::vector<Range>& server_range = Postoffice::Get()->GetServerKeyRanges();
+  int server = idx % server_range.size();
+  Key k = server_range[server].end() - idx - 1;
+  return k;
+}
+
+void Worker::_pushData(Key idx, float* vals, int len, std::vector<int>& timestamp) {
+  auto cb = getCallBack<DensePush>();
+  PSFData<DensePush>::Request request(mapWkeyToSkey(idx), len, SArray<float>(vals, len));
+  int ts = _kvworker.Request<DensePush>(request, cb);
+  timestamp.push_back(ts);
+}
+
+void Worker::_pullData(Key idx, float* vals, int len, std::vector<int>& timestamp) {
+  auto cb = getCallBack<DensePull>(SArray<float>(vals, len));
+  PSFData<DensePull>::Request request(mapWkeyToSkey(idx), len);
+  int ts = _kvworker.Request<DensePull>(request, cb);
+  timestamp.push_back(ts);
 }
 
 Worker worker;

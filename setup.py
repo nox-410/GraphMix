077d76b588b69427102e65158606649677aa1a46
Shi Yining <1700012728@pku.edu.cn>
2021-04-06 15:04:39 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="graphmix",
+    version="0.0.1",
+    author="Yining Shi",
+    author_email="shiyining@pku.edu.cn",
+    description="GraphMix : scalable minibatch server for GNN training",
+    # ext_modules=[CMakeExtension("graphmix")],
+    # cmdclass={"build_ext": CMakeBuild},
+    packages=['graphmix', 'graphmix.dataset', 'graphmix.tensorflow', 'graphmix.torch'],
+    package_dir = {'': 'python'},
+    zip_safe=False,
+)

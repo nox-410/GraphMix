f31c25e8d7edd172c34475016dd8388b71fc1162
Shi Yining <1700012728@pku.edu.cn>
2021-04-12 22:13:54 +0800

    update torch script

--- examples/torch_gcn.py
+++ examples/torch_gcn.py
@@ -3,9 +3,11 @@ import argparse
 import os
 import yaml
 import time
+import random
 
 import graphmix
 from graphmix.torch import SageConv, mp_matrix
+from graphmix.dataset import load_dataset
 import torch
 import torch.nn as nn
 import torch.distributed as dist
@@ -27,6 +29,13 @@ class Net(torch.nn.Module):
         x = self.classifier(x)
         return x
 
+def torch_sync_data(*args):
+    # all-reduce train stats
+    t = torch.tensor(args, dtype=torch.float64, device='cuda')
+    dist.barrier()  # synchronizes all processes
+    dist.all_reduce(t, op=torch.distributed.ReduceOp.SUM)
+    return t
+
 def worker_main(args):
     meta = args.meta
     dist.init_process_group(
@@ -44,10 +53,9 @@ def worker_main(args):
     comm = graphmix._C.get_client()
     query = comm.pull_graph()
     start = time.time()
-
-    from graphmix.dataset import load_dataset
-    dataset = load_dataset(meta["name"])
-    best_result = 0
+    if dist.get_rank() == 0:
+        dataset = load_dataset(meta["name"])
+        best_result = 0
     def eval_data():
         nonlocal best_result
         with torch.no_grad():
@@ -61,10 +69,9 @@ def worker_main(args):
             best_result = max(best_result, float(count/total))
             print(float(count/total), best_result)
             model.train()
-    import random
     samplers = [graphmix.sampler.LocalNode, graphmix.sampler.GlobalNode,
         graphmix.sampler.GraphSage, graphmix.sampler.RandomWalk]
-    for i in range(150):
+    for i in range(1000):
         random.shuffle(samplers)
         graph = comm.resolve(query)
         graph.add_self_loop()
@@ -79,27 +86,24 @@ def worker_main(args):
         out = model(x, graph)
         loss = F.cross_entropy(out, label, reduction='none')
         loss = loss * train_mask
-        loss = loss.mean()
+        total = train_mask.sum()
+        loss = loss.sum() / total
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        eval_mask = y[:,1]==0
-        count = int(((out.argmax(axis=1) == label)*eval_mask).sum())
-        total = eval_mask.sum()
 
-        # all-reduce train stats
-        t = torch.tensor([count, total, float(loss) / dist.get_world_size()], dtype=torch.float64, device='cuda')
-        dist.barrier()  # synchronizes all processes
-        dist.all_reduce(t, op=torch.distributed.ReduceOp.SUM)
+        count = int(((out.argmax(axis=1) == label)*train_mask).sum())
+        count, total, loss = torch_sync_data(count, total, loss)
         if dist.get_rank() == 0:
-            print("epoch={} loss={:.5f} acc={:.5f}".format(i, t[2], t[0]/t[1]))
-            eval_data()
+            print("batchnum={} loss={:.5f} acc={:.5f}".format(i, loss / dist.get_world_size(), count/total))
+            if i % 100 == 0 and i > 0:
+                eval_data()
 
 def server_init(server):
-    #server.init_cache(1, graphmix.cache.LFUOpt)
-    server.add_sampler(graphmix.sampler.LocalNode, batch_size=512)
-    #server.add_sampler(graphmix.sampler.GraphSage, batch_size=12, depth=2, width=2)
-    #server.add_sampler(graphmix.sampler.RandomWalk, rw_head=1024, rw_length=2)
+    server.init_cache(1, graphmix.cache.LFUOpt)
+    server.add_sampler(graphmix.sampler.LocalNode, batch_size=2000)
+    server.add_sampler(graphmix.sampler.GraphSage, batch_size=400, depth=2, width=2)
+    server.add_sampler(graphmix.sampler.RandomWalk, rw_head=1000, rw_length=2)
     #server.add_sampler(graphmix.sampler.GlobalNode, batch_size=2708)
     server.is_ready()
 

4f500e9cff0384e013b7d378b585abda63e03030
Shi Yining <1700012728@pku.edu.cn>
2021-05-03 22:59:13 +0800

    Refine import C++ module

--- python/graphmix/__init__.py
+++ python/graphmix/__init__.py
@@ -1,10 +1,3 @@
-def __handle_import():
-    import sys
-    import os
-    cur_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-    lib_path = os.path.join(cur_path, '../../build/lib/')
-    sys.path.append(lib_path)
-__handle_import()
 from libc_graphmix import cache
 from libc_graphmix import sampler
 from libc_graphmix import Graph
